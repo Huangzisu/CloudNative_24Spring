6d18d7d4093209d301adfdf62ac18a5069c44bea
ShangYu <2026967059@qq.com>
2024-03-21 16:38:43 +0800

    contribute func 3 & 4

---
+++ ase_contribute_service/src/main/java/org/fd/ase/grp15/ase_contribute_service/entity/vo/ListContribution.java
@@ -0,0 +1,37 @@
+package org.fd.ase.grp15.ase_contribute_service.entity.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.fd.ase.grp15.ase_contribute_service.entity.Contribution;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ListContribution implements Serializable {
+    private long id;
+    private String conferenceName;
+
+    private String time;
+
+    private int status;
+
+    private String title;
+
+    public ListContribution(long id, String conferenceName, String time, String title, int status){
+        this.id = id;
+        this.conferenceName = conferenceName;
+        this.time = time;
+        this.title = title;
+        this.status = status;
+    }
+
+    public ListContribution(Contribution contribution) {
+        this.id = contribution.getId();
+        this.conferenceName = contribution.getConferenceName();
+        this.time = contribution.getContributeTime();
+        this.title  = contribution.getTitle();
+        this.status = contribution.getStatus();
+    }
+
+}
